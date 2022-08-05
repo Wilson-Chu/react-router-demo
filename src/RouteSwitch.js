39fd70645f819998e222bf497f9fb14ee94450e3
Wilson-Chu <wilson1.chu@gmail.com>
2022-08-04 20:51:39 -0400

    updated App & RouteSwitch components, added Error component

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -2,17 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Profile from "./Profile";
 import Contact from "./Contact";
+import Error from "./Error";
 
 const RouteSwitch = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default RouteSwitch;
